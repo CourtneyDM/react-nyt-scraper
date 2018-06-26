e19db2cedc39042996c29d847daa644bc3e9ae80
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-06-26 09:31:57 -0500

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,10 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            topic: '',
-            startYear: '',
-            endYear: ''
+            query: '',
+            start_date: '',
+            end_date: '',
+            limit: 5
         }
 
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
@@ -27,7 +28,7 @@ class App extends Component {
 
     handleFormSubmit = event => {
         event.preventDefault();
-        alert(`States: ${this.state.topic}, ${this.state.startYear}, ${this.state.endYear}`);
+        alert(`States: ${this.state.query}, ${this.state.start_date}, ${this.state.end_date}`);
         // TODO: Here is where we will make the call to scrape the NYT site
     }
 
@@ -41,21 +42,21 @@ class App extends Component {
                 <Section name='search'>
                     <h3>Search</h3>
                     <form>
-                        <label htmlFor='topic'>Topic</label>
+                        <label htmlFor='query'>Topic</label>
                         <Input
-                            name='topic'
-                            placeholder='Enter topic to search (required)'
+                            name='query'
+                            placeholder='Enter query to search (required)'
                             onChange={ this.handleInputChange }
                         />
-                        <label htmlFor='startYear'>Start Year</label>
+                        <label htmlFor='start_date'>Start Year</label>
                         <Input
-                            name='startYear'
+                            name='start_date'
                             placeholder='Enter Start Year (required)'
                             onChange={ this.handleInputChange }
                         />
-                        <label htmlFor='endYear'>End Year</label>
+                        <label htmlFor='end_date'>End Year</label>
                         <Input
-                            name='endYear'
+                            name='end_date'
                             placeholder='Enter an End Year (required)'
                             onChange={ this.handleInputChange }
                         />
--- controllers/routes.js
+++ controllers/routes.js
@@ -10,11 +10,45 @@ const db = require('../models');
 const router = express.Router();
 
 // Read all saved articles and send to client
-router.get('/articles', (req, res) => {
+router.get('/api/articles', (req, res) => {
+    console.log(req);
+
     db.Article.find()
         .sort({ date: -1 })
         .then(dbArticle => {
             return res.json(dbArticle);
         })
         .catch(error => res.json(error));
+});
+
+// Save an article to the database
+router.post('/api/articles', (req, res) => {
+    const article = req.body;
+
+    // Update database with scraped article
+    db.Article.updateMany(
+        {
+            title: article.title,
+            url: article.url,
+            date: article.date
+        },
+        {
+            $set:
+            {
+                title: article.title,
+                url: article.url,
+                date: article.url
+            }
+        },
+        { upsert: true })
+        .then(result => {
+            console.log(result);
+        })
+        .catch(error => console.log(error));
+    return res.send('Updated.');
+});
+
+// Delete an article from the database
+router.delete('/api/articles', (req, res) => {
+
 });
